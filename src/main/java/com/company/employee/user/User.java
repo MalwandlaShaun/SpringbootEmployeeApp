d709453d4a51c3412163a87085fa314dbbb8273d
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2025-03-08 07:43:36 +0200

    first commit

---
+++ src/main/java/com/company/employee/user/User.java
@@ -0,0 +1,28 @@
+package com.company.employee.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
